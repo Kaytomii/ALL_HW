18db987b2bce46513d6b01c05239ca3806422a59
Yan Tarasov <vzu3427@gmail.com>
2025-11-25 16:59:37 +0200

    Added HW14

---
+++ HW14/List.h
@@ -0,0 +1,127 @@
+﻿#pragma once
+#include "Node.h"
+#include <iostream>
+
+template <typename T>
+class List
+{
+private:
+    Node<T>* head;
+    unsigned int size;
+
+public:
+    List();
+    ~List();
+
+    void AddToHead(const T& value);
+    void AddToTail(const T& value);
+
+    void DeleteFromHead();
+    void DeleteFromTail();
+
+    void DeleteAll();
+
+    void Show() const;
+
+    unsigned int GetSize() const;
+};
+
+
+template <typename T>
+List<T>::List() : head(nullptr), size(0) {}
+
+template <typename T>
+List<T>::~List()
+{
+    DeleteAll();
+}
+
+template <typename T>
+void List<T>::AddToHead(const T& value)
+{
+    Node<T>* newNode = new Node<T>(value);
+    newNode->next = head;
+    head = newNode;
+    ++size;
+}
+
+template <typename T>
+void List<T>::AddToTail(const T& value)
+{
+    Node<T>* newNode = new Node<T>(value);
+    if (!head)
+    {
+        head = newNode;
+    }
+    else
+    {
+        Node<T>* current = head;
+        while (current->next)
+        {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+    ++size;
+}
+
+template <typename T>
+void List<T>::DeleteFromHead()
+{
+    if (!head) return;
+
+    Node<T>* temp = head;
+    head = head->next;
+    delete temp;
+    --size;
+}
+
+template <typename T>
+void List<T>::DeleteFromTail()
+{
+    if (!head) return;
+
+    if (!head->next)
+    {
+        delete head;
+        head = nullptr;
+    }
+    else
+    {
+        Node<T>* current = head;
+        while (current->next->next)
+        {
+            current = current->next;
+        }
+        delete current->next;
+        current->next = nullptr;
+    }
+    --size;
+}
+
+template <typename T>
+void List<T>::DeleteAll()
+{
+    while (head)
+    {
+        DeleteFromHead();
+    }
+}
+
+template <typename T>
+void List<T>::Show() const
+{
+    Node<T>* current = head;
+    while (current)
+    {
+        std::cout << current->data << " ";
+        current = current->next;
+    }
+    std::cout << std::endl;
+}
+
+template <typename T>
+unsigned int List<T>::GetSize() const
+{
+    return size;
+}
---
+++ HW14/Source_for_HW14.cpp
@@ -0,0 +1,23 @@
+﻿#include "List.h"
+
+int main()
+{
+    List<int> myList;
+
+    myList.AddToHead(10);
+    myList.AddToTail(20);
+    myList.AddToTail(30);
+
+    myList.Show(); // 10 20 30
+
+    myList.DeleteFromHead();
+    myList.Show(); // 20 30
+
+    myList.DeleteFromTail();
+    myList.Show(); // 20
+
+    myList.DeleteAll();
+    myList.Show();
+
+    return 0;
+}
