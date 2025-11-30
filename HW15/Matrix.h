db553d0863d70e5f94cb2cc9caaa9f3a549ed567
Yan Tarasov <vzu3427@gmail.com>
2025-11-30 17:03:59 +0200

    Added HW15

---
+++ HW15/Matrix.h
@@ -0,0 +1,134 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+#include <ctime>
+
+template <typename T>
+class Matrix 
+{
+    int rows, cols;
+    std::vector<std::vector<T>> data;
+
+public:
+    Matrix(int r, int c) : rows(r), cols(c), data(r, std::vector<T>(c)) {}
+
+    void fill_input()
+    {
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < cols; ++j)
+            {
+                std::cin >> data[i][j];
+            }
+        }
+    }
+
+    void fill_random(T min_val, T max_val) 
+    {
+        srand(time(0));
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < cols; ++j)
+            {
+                data[i][j] = min_val + rand() % (int)(max_val - min_val + 1);
+            }
+        }
+    }
+
+    void display() const 
+    {
+        for (int i = 0; i < rows; ++i) 
+        {
+            for (int j = 0; j < cols; ++j)
+            {
+                std::cout << data[i][j] << " ";
+            }
+
+            std::cout << "\n";
+        }
+    }
+
+    Matrix operator+(const Matrix& other) const 
+    {
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < cols; ++j)
+            {
+                result.data[i][j] = data[i][j] + other.data[i][j];
+            }  
+        }
+
+        return result;
+    }
+
+    Matrix operator-(const Matrix& other) const
+    {
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < cols; ++j)
+            {
+                result.data[i][j] = data[i][j] - other.data[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    Matrix operator*(const Matrix& other) const 
+    {
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < cols; ++j)
+            {
+                result.data[i][j] = data[i][j] * other.data[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    Matrix operator/(const Matrix& other) const 
+    {
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < cols; ++j)
+            {
+                result.data[i][j] = data[i][j] / other.data[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    T min_element() const 
+    {
+        T minVal = data[0][0];
+        for (const auto row : data)
+        {
+            for (const auto el : row)
+            {
+                if (el < minVal) minVal = el;
+            }
+        }
+
+        return minVal;
+    }
+
+    T max_element() const 
+    {
+        T maxVal = data[0][0];
+        for (const auto& row : data)
+        {
+            for (const auto& el : row)
+            {
+                if (el > maxVal) maxVal = el;
+            }
+        }
+
+        return maxVal;
+    }
+};
---
+++ HW15/Source_for_HW15.cpp
@@ -0,0 +1,25 @@
+﻿#include "Matrix.h"
+
+int main() 
+{
+    setlocale(LC_ALL, "");
+
+    Matrix<int> A(3, 3), B(3, 3);
+
+    A.fill_random(1, 9);
+    B.fill_random(1, 9);
+
+    std::cout << "Matrix A:\n";
+    A.display();
+
+    std::cout << "Matrix B:\n";
+    B.display();
+
+    Matrix<int> C = A + B;
+    std::cout << "A + B:\n";
+    C.display();
+
+    std::cout << "Min in A: " << A.min_element() << "\n";
+    std::cout << "Max in A: " << A.max_element() << "\n";
+
+}
